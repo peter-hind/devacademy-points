0ebd854084ffc98f57cc578e1d7d096f2fea9413
Peter Hind <peter.hind47@gmail.com>
2023-11-17 11:08:03 +1300

    add nav bar and update student migrations to have a profile pic

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -6,6 +6,7 @@ import { useQuery } from '@tanstack/react-query'
 import { getProfiles } from '../apis/profile'
 import { useProfiles } from '../hooks/useProfiles.ts'
 import { Route, Routes, Outlet } from 'react-router-dom'
+import Nav from './Nav.tsx'
 
 function App() {
   //Pete - call useQuery/useProfiles hook to get all student profiles by calling API getProfiles function
@@ -14,7 +15,7 @@ function App() {
     <div className="app">
       <div className="container">
         <Heading />
-        {/* <Nav /> */}
+        <Nav />
         <Outlet />
       </div>
       <div className="tile-bottom"></div>
