d1c3feab7aa17cf281200186205cfbe46cd0d5ef
Teancum03 <144631723+Teancum03@users.noreply.github.com>
2023-11-17 11:19:25 +1300

    yuh

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,9 +1,73 @@
-export default function Form() {
-  return (
+import React, { useState } from 'react';
+import Profile from './Profile';
+import SingleComment from './SingleComment';
+import { StudentData } from '../../models/profile';
+import { Comment } from '../../models/comment';
 
-      <div className="box form">
-        <h1>Form</h1>
-      </div>
-
-  )
+interface FormProps {
+  student: StudentData;
 }
+
+const Form: React.FC<FormProps> = ({ student }) => {
+  const [points, setPoints] = useState<number>(0);
+  const [comment, setComment] = useState<string>('');
+
+  const handlePointsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPoints(Number(e.target.value));
+  };
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value);
+  };
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newComment: Comment = {
+      comment_content: comment,
+      student_name: student.student_name,
+      teacher_name: 'Your Teacher', // Change this 
+      points: points,
+      comment_id: Date.now().toString(), // Generate a unique ID
+    };
+
+    
+
+    setPoints(0);
+    setComment('');
+  };
+
+  return (
+    <div>
+      <Profile {...student} />
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Points:
+          <input type="number" value={points} onChange={handlePointsChange} />
+        </label>
+        <br />
+        <label>
+          Comment:
+          <input type="text" value={comment} onChange={handleCommentChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {points !== 0 && comment && (
+        <SingleComment
+          student_name={student.student_name}
+          teacher_name="Your Teacher"  // Change this 
+          points={points}
+          comment_content={comment}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Form;
+
+
+
+
+
