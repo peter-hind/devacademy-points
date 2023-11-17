1240ab770a634bdc88cf0d41df99ebdb86ce3b35
Teancum03 <144631723+Teancum03@users.noreply.github.com>
2023-11-17 14:02:40 +1300

    made teacher img

--- server/db/seeds/teachers.js
+++ server/db/seeds/teachers.js
@@ -3,11 +3,11 @@ export async function seed(knex) {
 
   // Inserts seed entries
   await knex('teachers').insert([
-    { teacher_id: 1, teacher_name: 'Jatin', favorite_thing: 'Star Wars' },
-    { teacher_id: 2, teacher_name: 'Jen', favorite_thing: 'Cats' },
-    { teacher_id: 3, teacher_name: 'David', favorite_thing: 'Board Games' },
-    { teacher_id: 4, teacher_name: 'DaviD', favorite_thing: 'Avatar' },
-    { teacher_id: 5, teacher_name: 'Gerard', favorite_thing: 'Ghosts' },
-    { teacher_id: 6, teacher_name: 'Joseph', favorite_thing: 'Vaping' },
+    { teacher_id: 1, teacher_name: 'Jatin', favorite_thing: 'Star Wars', teacher_picture:'' },
+    { teacher_id: 2, teacher_name: 'Jen', favorite_thing: 'Cats' , teacher_picture:''},
+    { teacher_id: 3, teacher_name: 'David', favorite_thing: 'Board Games' , teacher_picture:''},
+    { teacher_id: 4, teacher_name: 'DaviD', favorite_thing: 'Avatar' , teacher_picture:''},
+    { teacher_id: 5, teacher_name: 'Gerard', favorite_thing: 'Ghosts' , teacher_picture:''},
+    { teacher_id: 6, teacher_name: 'Joseph', favorite_thing: 'Vaping' , teacher_picture:''},
   ])
 }
